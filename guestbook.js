f9b0ab226af38df41382e14984ef707a89b28c75
Aaron Wells <56880210+awells2399@users.noreply.github.com>
2022-02-23 15:23:02 -0500

    Update guestbook.js
    Updated apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9eba5b95.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
